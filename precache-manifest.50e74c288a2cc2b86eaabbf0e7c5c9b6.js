977ee90e6cdddf71ff1ea188d4b69192d082e052
Roger <thrasher_royerr666@hotmail.com>
2020-03-04 17:51:26 -0600

    Updates

---
+++ precache-manifest.50e74c288a2cc2b86eaabbf0e7c5c9b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "881f81326c93048fa19e92e4d3c9dff3",
+    "url": "/Reactjs-gameapi-deploy/index.html"
+  },
+  {
+    "revision": "2513cc20a694052a14b9",
+    "url": "/Reactjs-gameapi-deploy/static/css/main.4c6651ee.chunk.css"
+  },
+  {
+    "revision": "509b3e2ebaab4dfbacbe",
+    "url": "/Reactjs-gameapi-deploy/static/js/2.fa57dd03.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/Reactjs-gameapi-deploy/static/js/2.fa57dd03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2513cc20a694052a14b9",
+    "url": "/Reactjs-gameapi-deploy/static/js/main.0efcd1f0.chunk.js"
+  },
+  {
+    "revision": "02790742c68ce1ddf34e",
+    "url": "/Reactjs-gameapi-deploy/static/js/runtime-main.fa2fab03.js"
+  }
+]);
